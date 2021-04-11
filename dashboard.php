6f91b9c7796673ea13a5ba7b82ff2a661817302b
Alicia Mena <aliciamena12@gmail.com>
2021-04-11 04:08:55 +0200

    Modified Dashboard view

--- dashboard.php
+++ dashboard.php
@@ -17,26 +17,72 @@
   }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
-    <meta name="description" content="Practice of Drag and Drop">
-    <link rel="stylesheet" href="styles.css">
-    <!-- <link rel="icon" href="images\logo.png" type="image/png" sizes="16x16"> -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Raleway&display=swap" rel="stylesheet">
-   
-</head>
-<body>
 
-    <?php require 'partials/header.html' ?>
-        <br> Welcome. <?= $user['email']; ?>
-        <br>You are successfully Logged In.
-        <a href="logout.php">Logout</a>
+<?php include("includes/header.html") ?>
+	<?= $user['email']; ?>
+	<a class="links" href="logout.php">Logout</a>
 
-</body>
-</html>
+<main>
+	<h1 class="title">Productivity</h1>
+
+	<section class="list__container" id="list__container">
+		<div class="listbox">
+			<div class="list__name" contenteditable="true">
+				<p id="listname" onkeyup="edit()">List 1</p>
+			</div>                
+			<ul class="list" id="list1">
+				<li class="list__element">Element 1</li>
+				<li class="list__element">Element 2</li>
+			</ul>
+			<form action="save_task.php" method="POST" class="element__add">
+				<input class="task__add" type="text" name="title" placeholder="Add">
+				<input class="add__button" type="submit" name="save_task" value="+">
+			</form>
+		</div>
+
+		<div class="listbox">
+			<div class="list__name" contenteditable="true">
+				<p>List 2</p>
+			</div>                
+			<ul class="list" id="list2">
+				<li class="list__element">Element 1</li>
+				<li class="list__element">Element 2</li>
+			</ul> 
+			<form action="save_task.php" method="POST" class="element__add">
+				<input class="task__add" type="text" name="title" placeholder="Add">
+				<input class="add__button" type="submit" name="save_task" value="+">
+			</form>
+		</div>
+
+		<div class="listbox">
+			<div class="list__name" contenteditable="true">
+				<p>List 3</p>
+			</div>                
+			<ul class="list" id="list3">
+				<li class="list__element">Element 1</li>
+				<li class="list__element">Element 2</li>
+			</ul>
+			<form action="save_task.php" method="POST" class="element__add">
+				<input class="task__add" type="text" name="title" placeholder="Add">
+				<input class="add__button" type="submit" name="save_task" value="+">
+			</form>
+		</div>
+
+		<div class="listbox">
+			<div class="list__name" contenteditable="true">
+				<p>List 4</p>
+			</div>                
+			<ul class="list" id="list4">
+				<li class="list__element">Element 1</li>
+				<li class="list__element">Element 2</li>
+			</ul>
+			<form action="save_task.php" method="POST" class="element__add">
+				<input class="task__add" type="text" name="title" placeholder="Add">
+				<input class="add__button" type="submit" name="save_task" value="+">
+			</form>
+		</div>
+	</section>
+</main>
+
+<?php include("includes/footer.html") ?>
+
